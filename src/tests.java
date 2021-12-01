72d17f0c7bed354f1ae5bdba06bff65dc716ef61
alexandrazolman <zolman21@wfu.edu>
2021-12-01 14:31:09 -0500

    Start

--- src/tests.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class tests {
-    public static void main(String[] args) {
-        Scanner gelatin = new Scanner(System.in);
-        String userInput = gelatin.next();
-        System.out.println(userInput);
-    }
-}
